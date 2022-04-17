b948b964918916e5f6f13b205a97c0681e5dc461
lenovo <houssam.ZINEDDINE@student.isae-supaero.fr>
2022-04-17 18:16:28 +0200

    Création des tables itinéraires et topoguide

--- topoguide/itineraires/admin.py
+++ topoguide/itineraires/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Itineraire,Sortie,Experience,Niveau,Meteo
+
+admin.site.register(Itineraire)
+admin.site.register(Sortie)
 
 # Register your models here.
--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+class Niveau(models.Model):
+    niveau=models.IntegerField('Niveau')
+    def __str__(self):
+        return str(self.niveau) 
+class Itineraire(models.Model):
+    titre = models.CharField(max_length=200)
+    point_depart=models.CharField(max_length=200)
+    description=models.TextField()
+    altitude_depart=models.IntegerField('Altitude (m) ')
+    altitude_max=models.IntegerField('Altitude max (m) ')
+    altitude_min=models.IntegerField('Altitude min (m) ')
+    denivele_positif=models.IntegerField( 'Dénivelé positif (m) ')
+    denivele_negatif = models.IntegerField('Dénivelé négatif (m) ')
+    duree_estimee=models.FloatField('Durée (h) ')
+    difficulte_estimee=models.ForeignKey('Niveau',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.titre
+class Experience(models.Model):
+    choix=models.CharField(max_length=100)
+    def __str__(self):
+        return self.choix
+    
+class Meteo(models.Model):
+    choix=models.CharField(max_length=100)
+    def __str__(self):
+        return self.choix
+   
+class Sortie(models.Model):
+    utilisateur=models.ForeignKey(User,on_delete=models.CASCADE)
+    itineraire = models.ForeignKey('Itineraire',on_delete=models.CASCADE)
+    date_sortie=models.DateField('Date de sortie')
+    duree_reelle=models.FloatField('Durée réelle')
+    nombre_personne=models.IntegerField('Nombres de personnnes')
+    experience=models.ForeignKey('Experience',on_delete=models.CASCADE)
+    meteo=models.ForeignKey('Meteo',on_delete=models.CASCADE)
+    difficulte_ressentie=models.ForeignKey('Niveau',on_delete=models.CASCADE)
+    
+    
+    
+    
+    
+        
 # Create your models here.
