a960394cb25f0f42098d6da5bf0aec06501358d0
lenovo <houssam.ZINEDDINE@student.isae-supaero.fr>
2022-04-18 17:01:35 +0200

    Affichage de la première page des itinéraires

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from .models import Itineraire,Sortie
 
+def itineraire_list(request):
+    list_itineraire=Itineraire.objects.order_by('id')
+    return render(request,'itineraires/itineraire_list.html',{'list_itineraire' : list_itineraire})
 # Create your views here.
